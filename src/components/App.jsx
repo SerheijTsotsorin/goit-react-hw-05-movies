f7c48521951aafbb56102badbea815784cccbaeb
SerheijTsotsorin <84991425+SerheijTsotsorin@users.noreply.github.com>
2022-04-16 10:17:25 +0300

    add navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,31 @@
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         textTransform: 'uppercase',
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from 'components/Layout/Layout';
+// import { HomePage } from 'components/HomePage/HomePage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* <Route index element={<HomePage />} /> */}
+      </Route>
+    </Routes>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// import { BrowserRouter } from "react-router-dom";
+// import "./index.css";
+// import {App} from "components/App";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+//   </React.StrictMode>,  
+//   document.getElementById("root")
+// );
